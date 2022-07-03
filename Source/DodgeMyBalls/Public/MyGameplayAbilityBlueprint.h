8ce89cc82d7ab2f91ae58b70f2423bee7461916f
Vincent Payeur <payeurv@hotmail.com>
2022-07-03 19:44:35 -0400

    Continuation des tests du Gameplay abilities

---
+++ Source/DodgeMyBalls/Public/MyGameplayAbilityBlueprint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayAbilityBlueprint.h"
+#include "MyGameplayAbilityBlueprint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DODGEMYBALLS_API UMyGameplayAbilityBlueprint : public UGameplayAbilityBlueprint
+{
+	GENERATED_BODY()
+	
+};
